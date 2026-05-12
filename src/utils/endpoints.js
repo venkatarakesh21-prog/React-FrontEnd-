3593b9aac81dc5e9feb04cbc2afaf01e649c9fdb
venkatarakesh21-prog <venkatarakesh21@gmail.com>
2026-05-12 00:13:27 -0700

    CHAT

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -4,8 +4,6 @@ const ENDPOINTS = {
     DETAILS: "/History/GetChatDetails",
     SEND_MESSAGE: "/Chat/SendMessage",
     DELETE_CHAT: "/History/DeleteChat", // make sure this matches your backend route
-    UPDATE_TITLE: "/History/UpdateChatTitle", // new endpoint for updating chat title
-    UPDATE_MESSAGE: "/History/UpdateMessage", // new endpoint for updating a message
   },
   AUTH: {
     LOGIN: "/Auth/login",
